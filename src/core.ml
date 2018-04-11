7aced5811d8994cdf5897fceaf482a68f3853a69
Arto Bendiken <arto@dryproject.org>
2018-04-11 13:28:57 +0300

    Defined some more core types.

--- src/core.ml
+++ src/core.ml
@@ -8,9 +8,9 @@ module Char = struct
   type t = int
 end
 
-module Datum = struct
-  type t (* TODO *)
-end
+(*module Complex*)
+
+(*module Datum*)
 
 module Float32 = struct
   type t = float
@@ -20,7 +20,11 @@ module Float64 = struct
   type t = float
 end
 
-module Float = Float64
+module Float = struct
+  type t =
+    | Float32 of Float32.t
+    | Float64 of Float64.t
+end
 
 module Int8 = struct
   type t = int
@@ -39,10 +43,17 @@ module Int64 = struct
 end
 
 module Int128 = struct
-  type t (* TODO *)
+  type t = { value: int64 } (* TODO *)
 end
 
-module Int = Int64
+module Int = struct
+  type t =
+    | Int8 of Int8.t
+    | Int16 of Int16.t
+    | Int32 of Int32.t
+    | Int64 of Int64.t
+    | Int128 of Int128.t
+end
 
 module Integer = struct
   type t = { value: int64 } (* TODO *)
@@ -60,9 +71,7 @@ module Natural = struct
   type t = Integer.t
 end
 
-module Number = struct
-  type t (* TODO *)
-end
+(*module Number*)
 
 module Quantity = struct
   type t (* TODO *)
@@ -76,17 +85,13 @@ module Real = struct
   type t = { value: float } (* TODO *)
 end
 
-module Scalar = struct
-  type t (* TODO *)
-end
+(*module Scalar*)
 
 module Symbol = struct
   type t = string
 end
 
-module Tensor = struct
-  type t (* TODO *)
-end
+(*module Tensor*)
 
 module Unit = struct
   type t (* TODO *)
@@ -112,8 +117,48 @@ module Word64 = struct
   type t = int64
 end
 
-module Word = Word64
+module Word = struct
+  type t =
+    | Word8 of Word8.t
+    | Word16 of Word16.t
+    | Word32 of Word32.t
+    | Word64 of Word64.t
+end
 
 module Complex = struct
   type t = { real: Real.t; imaginary: Real.t }
+end
+
+module Number = struct
+  type t =
+    | Complex of Complex.t
+    | Float of Float.t
+    | Int of Int.t
+    | Integer of Integer.t
+    | Natural of Natural.t
+    | Rational of Rational.t
+    | Real of Real.t
+end
+
+module Scalar = struct
+  type t =
+    | Bool of Bool.t
+    | Char of Char.t
+    | Number of Number.t
+end
+
+module Tensor = struct
+  type t =
+    | Scalar of Scalar.t
+    | Vector of Vector.t
+    | Matrix of Matrix.t
+end
+
+module Datum = struct
+  type t =
+    | Interval of Interval.t
+    | Quantity of Quantity.t
+    | Symbol of Symbol.t
+    | Tensor of Tensor.t
+    | Unit of Unit.t
 end
--- src/core.mli
+++ src/core.mli
@@ -12,9 +12,7 @@ module Complex : sig
   type t
 end
 
-module Datum : sig
-  type t
-end
+(*module Datum*)
 
 module Float32 : sig
   type t = float
@@ -24,7 +22,11 @@ module Float64 : sig
   type t = float
 end
 
-module Float = Float64
+module Float : sig
+  type t =
+    | Float32 of Float32.t
+    | Float64 of Float64.t
+end
 
 module Int8 : sig
   type t = int
@@ -46,7 +48,14 @@ module Int128 : sig
   type t
 end
 
-module Int = Int64
+module Int : sig
+  type t =
+    | Int8 of Int8.t
+    | Int16 of Int16.t
+    | Int32 of Int32.t
+    | Int64 of Int64.t
+    | Int128 of Int128.t
+end
 
 module Integer : sig
   type t
@@ -64,9 +73,7 @@ module Natural : sig
   type t = Integer.t
 end
 
-module Number : sig
-  type t
-end
+(*module Number*)
 
 module Quantity : sig
   type t
@@ -80,17 +87,13 @@ module Real : sig
   type t
 end
 
-module Scalar : sig
-  type t
-end
+(*module Scalar*)
 
 module Symbol : sig
   type t = string
 end
 
-module Tensor : sig
-  type t
-end
+(*module Tensor*)
 
 module Unit : sig
   type t
@@ -116,4 +119,44 @@ module Word64 : sig
   type t = int64
 end
 
-module Word = Word64
+module Word : sig
+  type t =
+    | Word8 of Word8.t
+    | Word16 of Word16.t
+    | Word32 of Word32.t
+    | Word64 of Word64.t
+end
+
+module Number : sig
+  type t =
+    | Complex of Complex.t
+    | Float of Float.t
+    | Int of Int.t
+    | Integer of Integer.t
+    | Natural of Natural.t
+    | Rational of Rational.t
+    | Real of Real.t
+end
+
+module Scalar : sig
+  type t =
+    | Bool of Bool.t
+    | Char of Char.t
+    | Number of Number.t
+end
+
+module Tensor : sig
+  type t =
+    | Scalar of Scalar.t
+    | Vector of Vector.t
+    | Matrix of Matrix.t
+end
+
+module Datum : sig
+  type t =
+    | Interval of Interval.t
+    | Quantity of Quantity.t
+    | Symbol of Symbol.t
+    | Tensor of Tensor.t
+    | Unit of Unit.t
+end
