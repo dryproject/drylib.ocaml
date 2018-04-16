a60cbb9f5026989ca929696206835c7228f078c0
Arto Bendiken <arto@dryproject.org>
2018-04-16 16:30:55 +0300

    Defined types for Lua type/value representation.

--- src/code/Lua.ml
+++ src/code/Lua.ml
@@ -3,3 +3,97 @@
 (* See: https://www.lua.org/manual/5.3/manual.html#9 *)
 (* See: http://lua4z.com/doc/manual/syntax.md.html *)
 (* See: http://lua-users.org/wiki/LuaGrammar *)
+
+open DRY__Core
+
+module Boolean = DRY__Core.Bool
+
+module Float   = DRY__Core.Float
+
+module Integer = DRY__Core.Int
+
+module String  = DRY__Stdlib.String
+
+module Number = struct
+  type t = Integer of Integer.t | Float of Float.t
+
+  let of_float r = Float (Float.of_float r)
+
+  let of_int z = Integer (Integer.of_int z)
+
+  let to_string = function
+    | Integer z -> Integer.to_string z
+    | Float r -> Float.to_string r
+end
+
+module Function = struct
+  type t (* TODO *)
+end
+
+module Table = struct
+  type t (* TODO *)
+end
+
+module Value = struct
+  type t =
+    | Nil
+    | Boolean of Boolean.t
+    | Number of Number.t
+    | String of String.t
+    | Function (* TODO *)
+    | Userdata
+    | Thread
+    | Table (* TODO *)
+
+  let nil = Nil
+
+  let of_bool b = Boolean (Boolean.of_bool b)
+
+  let of_float r = Number (Number.of_float r)
+
+  let of_int z = Number (Number.of_int z)
+
+  let of_string s = String s
+
+  let to_string = function
+    | Nil -> "nil"
+    | Boolean b -> Boolean.to_string b
+    | Number n -> Number.to_string n
+    | String s -> s
+    | Function -> "<function>"
+    | Userdata -> "<userdata>"
+    | Thread -> "<thread>"
+    | Table -> "<table>" (* TODO *)
+end
+
+module Type = struct
+  type t =
+    | Nil
+    | Boolean
+    | Number
+    | String
+    | Function
+    | Userdata
+    | Thread
+    | Table
+
+  let of_value = function
+    | Value.Nil -> Nil
+    | Value.Boolean _ -> Boolean
+    | Value.Number _ -> Number
+    | Value.String _ -> String
+    | Value.Function -> Function
+    | Value.Userdata -> Userdata
+    | Value.Thread -> Thread
+    | Value.Table -> Table
+
+  let to_string = function
+    | Nil -> "nil"
+    | Boolean -> "boolean"
+    | Number -> "number"
+    | String -> "string"
+    | Function -> "function"
+    | Userdata -> "userdata"
+    | Thread -> "thread"
+    | Table -> "table"
+end
--- src/code/Lua.mli
+++ src/code/Lua.mli
@@ -1,3 +1,67 @@
 (* This is free and unencumbered software released into the public domain. *)
 
 (** Lua *)
+
+module Boolean = DRY__Core.Bool
+
+module Float = DRY__Core.Float
+
+module Integer = DRY__Core.Int
+
+module String = DRY__Stdlib.String
+
+module Number : sig
+  type t = Integer of Integer.t | Float of Float.t
+
+  val of_float : float -> t
+
+  val of_int : int -> t
+
+  val to_string : t -> string
+end
+
+module Function : sig
+  type t
+end
+
+module Table : sig
+  type t
+end
+
+module Value : sig
+  type t =
+    | Nil
+    | Boolean of Boolean.t
+    | Number of Number.t
+    | String of String.t
+    | Function
+    | Userdata
+    | Thread
+    | Table
+
+  val nil : t
+
+  val of_bool : bool -> t
+
+  val of_float : float -> t
+
+  val of_int : int -> t
+
+  val to_string : t -> string
+end
+
+module Type : sig
+  type t =
+    | Nil
+    | Boolean
+    | Number
+    | String
+    | Function
+    | Userdata
+    | Thread
+    | Table
+
+  val of_value : Value.t -> t
+
+  val to_string : t -> string
+end
