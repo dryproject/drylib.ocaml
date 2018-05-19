33b375630e6188c6311c7fbda5c20a1ba2f50847
Arto Bendiken <arto@dryproject.org>
2018-05-19 18:27:41 +0300

    Sketched out some string predicates.

--- src/text/UTF8.ml
+++ src/text/UTF8.ml
@@ -1,1 +1,21 @@
 (* This is free and unencumbered software released into the public domain. *)
+
+module Stdlib = DRY__Stdlib
+
+module String = struct
+  type t = string
+
+  let ends_with_char s c =
+    let len = String.length s in
+    len > 0 && c = (String.get s (len - 1))
+
+  let starts_with_char s c =
+    let len = String.length s in
+    len > 0 && c = (String.get s 0)
+
+  let contains_char = Stdlib.String.contains
+
+  let contains_string s1 s2 =
+    try ignore (Str.search_forward (Str.regexp_string s2) s1 0); true
+    with Not_found -> false
+end
--- src/text/UTF8.mli
+++ src/text/UTF8.mli
@@ -1,3 +1,13 @@
 (* This is free and unencumbered software released into the public domain. *)
 
 (** UTF-8 text *)
+
+module String : sig
+  type t = string
+
+  val ends_with_char : t -> char -> bool
+  val starts_with_char : t -> char -> bool
+  val contains_char : t -> char -> bool
+
+  val contains_string : t -> t -> bool
+end
